0efc3aa556026f038de30075959cd91f28f7dd59
Cyber-Chaser <37996512+Cyber-Chaser@users.noreply.github.com>
2020-12-01 13:59:06 +0800

    Update process_capture.sh

--- process_capture.sh
+++ process_capture.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # 参考来源：https://www.cnblogs.com/xiaozi/p/9561543.html
+# 用于捕捉存活时间很短的异常进程，打印相关信息
 i=1
 while :
 do
-   tmp=`ps -ef|grep ping|grep -v grep|awk '{print $3}'`
+   tmp=`ps -ef|grep ping|grep -v grep|awk '{print $3}'` # $2是进程pid，$3是父进程pid
    #echo $tmp
    if test -z "$tmp"
    then
